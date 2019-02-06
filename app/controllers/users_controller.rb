2982e5847364e3674262cbdcb64d0200baa87645
Cheng0315 <csalvue@gmail.com>
2019-02-06 13:12:52 -0800

    add create action in users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,22 @@
 class UsersController < ApplicationController 
-  
+
   def new
     @user = User.new
   end
 
   def create
-    
+    @user = User.new(users_params)
+
+    if user.save 
+      session[:user_id] = @user.user_id
+      redirect_to root_path
+    else
+      redirect_to new_user_path
+    end
   end
 
 
-  
+
   private
 
   def users_params
