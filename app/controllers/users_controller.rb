4c961f182da860b4a46d88131fde3a768c7202ba
Cheng0315 <csalvue@gmail.com>
2019-02-06 13:26:42 -0800

    add session controller, fix bug in sessions routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController 
 
   def index
-    
+
   end
 
   def new
@@ -11,8 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(users_params)
 
-    if user.save 
-      session[:user_id] = @user.user_id
+    if @user.save 
+      session[:user_id] = @user.id
       redirect_to root_path
     else
       redirect_to new_user_path
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users/index'
+  root 'users#index'
   resources :users, only: [:index, :new, :create, :edit, :update, :destroy]
-  get '/signin' => 'sessions/new'
-  post '/signin' => 'sessions/create'
-  get '/logout' => 'sessions/logout'
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/logout' => 'sessions#logout'
 end
