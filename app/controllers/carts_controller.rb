3783ee76747a29481b8acdf76c0c54e907edb09b
Cheng0315 <csalvue@gmail.com>
2019-02-08 11:16:54 -0800

    add checkout action to cart_controller.rb

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -9,6 +9,21 @@ class CartsController < ApplicationController
   end
 
   def checkout
-    binding.pry
+    if current_cart && current_user
+      current_cart.each_with_index do |item, idx|
+        @cart_item = CartItem.create(quantity: params[:quantity][idx])
+        @cart = Cart.create()
+        @cart.cart_items << @cart_item
+        @item = Item.find(item['id'])
+        @item.cart_items << @cart_item
+        current_user.carts << @cart
+        session.delete :cart
+        flash[:notice] = "Thank you for shopping with Swift Kart! Your order has been placed and we will send you a notice when your order is shipped!"
+        redirect_to root_path and return 
+      end
+    else
+      flash[:notice] = "Please sign in or sign up to place your order"
+      redirect_to signin_path
+    end
   end
 end
