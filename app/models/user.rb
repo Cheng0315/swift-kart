e53e169252a8df624d2d1c787ab3958a0cb3a68a
Cheng0315 <csalvue@gmail.com>
2019-02-10 11:15:41 -0800

    add AR validates to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
   has_many :carts
   has_many :items
   has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
 end
