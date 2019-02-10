c21f3383d3aee9ade12076feac13bbe677fe82dc
Cheng0315 <csalvue@gmail.com>
2019-02-09 22:12:16 -0800

    add collection select and select tag to edit item form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 
-  helper_method :current_user, :current_cart, :guest_cart
+  helper_method :current_user, :current_cart, :guest_cart, :not_seller_item
   
   def current_cart
     @current_cart ||= Cart.find(session[:cart_id]) if session[:cart_id]
@@ -14,12 +14,12 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  private
-
   def not_seller_item(item)
     @seller = User.find(item['user_id'])
     @seller.id != current_user.id
   end
+
+  private
 
   def this_path(path)
     if path
