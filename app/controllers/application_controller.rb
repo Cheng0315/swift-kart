8ae85cc09c902ed647d1a22c7bf3c1b9c79750a4
Cheng0315 <csalvue@gmail.com>
2019-02-11 17:21:49 -0800

    refactor create action in session controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -117,6 +117,29 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def signin_with_signin_form
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to cart_user_cart_path
+    else
+      flash[:notice] = "Invalid email or password"
+      redirect_to signin_path
+    end
+  end
+
+  def signin_with_facebook
+    @user = User.find_or_create_by(uid: auth['uid']) do |u|
+      u.first_name = auth['info']['name'].split[0]
+      u.last_name = auth['info']['name'].split[1] 
+      u.email = auth['info']['email']
+      u.password = auth['info']['email']
+      u.seller = false
+    end
+    session[:user_id] = @user.id
+    redirect_to cart_user_cart_path
+  end
+
   def redirect_path(path)
     if path
       path
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,25 +7,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if params[:email] || params[:password]
-      @user = User.find_by(email: params[:email])
-      if @user && @user.authenticate(params[:password])
-        session[:user_id] = @user.id
-        redirect_to cart_user_cart_path
-      else
-        flash[:notice] = "Invalid email or password"
-        redirect_to signin_path
-      end
+    if params[:email] 
+      signin_with_signin_form
     else
-      @user = User.find_or_create_by(uid: auth['uid']) do |u|
-        u.first_name = auth['info']['name'].split[0]
-        u.last_name = auth['info']['name'].split[1] 
-        u.email = auth['info']['email']
-        u.password = auth['info']['email']
-        u.seller = false
-      end
-      session[:user_id] = @user.id
-      redirect_to cart_user_cart_path
+      signin_with_facebook
     end
   end
 
