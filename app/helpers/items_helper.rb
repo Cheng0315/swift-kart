3fa1d8f46ab42a2e40866524e0dc32805b2fed38
Cheng0315 <csalvue@gmail.com>
2019-02-12 22:13:48 -0800

    add item_image helper method to display default image if no image is chosen when item is created

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -8,4 +8,14 @@ module ItemsHelper
   def item_is_shipped(cart_id, item_id)
     CartItem.find_by(cart_id: cart_id, item_id: item_id).shipped
   end
+
+  def item_image(item_id)
+    @item = Item.find(item_id)
+
+    if @item.image.attached?
+      image_tag(@item.image, style:"width:100%")
+    else
+      image_tag('default_image', style:"width:100%")
+    end
+  end
 end
