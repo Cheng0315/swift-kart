c4fb642c1da5db4ec4f9718276f4b204b2d54de6
Cheng0315 <csalvue@gmail.com>
2019-02-13 12:20:05 -0800

    add item_status to item helper file to display the status of the buyer orders

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -9,6 +9,11 @@ module ItemsHelper
     CartItem.find_by(cart_id: cart_id, item_id: item_id).shipped
   end
 
+  def item_status(item, cart)
+    @cart_item = CartItem.find_by(cart_id: cart.id, item_id: item.id)
+    @cart_item.shipped ? "Shipped! Your item is on its way!" : "Preparing to ship."
+  end
+
   def item_image(item_id)
     @item = Item.find(item_id)
 
