e31c6ec07eb2ab4c8e121b65adc955df49280b17
Cheng0315 <csalvue@gmail.com>
2019-02-15 00:02:14 -0800

    change x variable name in js app to quantity, change inside_idx to just idx

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,115 +27,127 @@
   var price9 = 0;
   var price10 = 0;
   
-  function total_price_1(inside_idx, item_price, all_items_price) {
-    var x = document.getElementById(`my_select_${inside_idx}`).value;
-    if (x == 1) {
+  function total_price_1(idx, item_price, all_items_price) {
+    var quantity = document.getElementById(`my_select_${idx}`).value;
+
+    if (quantity == 1) {
       price1 = 0;
     } else {
-      price1 = item_price * x - item_price;
+      price1 = item_price * quantity - item_price;
     }
     total_sums = all_items_price + price1 + price2 + price3 + price4 + price5 + price6 + price7 + price8 + price9 + price10;
-    document.getElementById("quantity_price").innerHTML = total_sums;
+    document.getElementById("quantity_price").innerHTML = "$" + total_sums.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
   }
 
-  function total_price_2(inside_idx, item_price, all_items_price) {
-    var x = document.getElementById(`my_select_${inside_idx}`).value;
-    if (x == 1) {
+  function total_price_2(idx, item_price, all_items_price) {
+    var quantity = document.getElementById(`my_select_${idx}`).value;
+
+    if (quantity == 1) {
       price2 = 0;
     } else {
-      price2 = item_price * x - item_price;
+      price2 = item_price * quantity - item_price;
     }
     total_sums = all_items_price + price1 + price2 + price3 + price4 + price5 + price6 + price7 + price8 + price9 + price10;
-    document.getElementById("quantity_price").innerHTML = total_sums;
+    document.getElementById("quantity_price").innerHTML = "$" + total_sums.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
   }
 
-  function total_price_3(inside_idx, item_price, all_items_price) {
-    var x = document.getElementById(`my_select_${inside_idx}`).value;
-    if (x == 1) {
+  function total_price_3(idx, item_price, all_items_price) {
+    var quantity = document.getElementById(`my_select_${idx}`).value;
+
+    if (quantity == 1) {
       price3 = 0;
     } else {
-      price3 = item_price * x - item_price;
+      price3 = item_price * quantity - item_price;
     }
     total_sums = all_items_price + price1 + price2 + price3 + price4 + price5 + price6 + price7 + price8 + price9 + price10;
-    document.getElementById("quantity_price").innerHTML = total_sums;
+    document.getElementById("quantity_price").innerHTML = "$" + total_sums.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
   }
 
-  function total_price_4(inside_idx, item_price, all_items_price) {
-    var x = document.getElementById(`my_select_${inside_idx}`).value;
-    if (x == 1) {
+  function total_price_4(idx, item_price, all_items_price) {
+    var quantity = document.getElementById(`my_select_${idx}`).value;
+
+    if (quantity == 1) {
       price4 = 0;
     } else {
-      price4 = item_price * x - item_price;
+      price4 = item_price * quantity - item_price;
     }
     total_sums = all_items_price + price1 + price2 + price3 + price4 + price5 + price6 + price7 + price8 + price9 + price10;
-    document.getElementById("quantity_price").innerHTML = total_sums;
+    document.getElementById("quantity_price").innerHTML = "$" + total_sums.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
   }
 
-  function total_price_5(inside_idx, item_price, all_items_price) {
-    var x = document.getElementById(`my_select_${inside_idx}`).value;
-    if (x == 1) {
+  function total_price_5(idx, item_price, all_items_price) {
+    var quantity = document.getElementById(`my_select_${idx}`).value;
+
+    if (quantity == 1) {
       price5 = 0;
     } else {
-      price5 = item_price * x - item_price;
+      price5 = item_price * quantity - item_price;
     }
     total_sums = all_items_price + price1 + price2 + price3 + price4 + price5 + price6 + price7 + price8 + price9 + price10;
-    document.getElementById("quantity_price").innerHTML = total_sums;
+    document.getElementById("quantity_price").innerHTML = "$" + total_sums.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
   }
 
-  function total_price_6(inside_idx, item_price, all_items_price) {
-    var x = document.getElementById(`my_select_${inside_idx}`).value;
-    if (x == 1) {
+  function total_price_6(idx, item_price, all_items_price) {
+    var quantity = document.getElementById(`my_select_${idx}`).value;
+
+    if (quantity == 1) {
       price6 = 0;
     } else {
-      price6 = item_price * x - item_price;
+      price6 = item_price * quantity - item_price;
     }
     total_sums = all_items_price + price1 + price2 + price3 + price4 + price5 + price6 + price7 + price8 + price9 + price10;
-    document.getElementById("quantity_price").innerHTML = total_sums;
+    document.getElementById("quantity_price").innerHTML = "$" + total_sums.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
   }
 
-  function total_price_7(inside_idx, item_price, all_items_price) {
-    var x = document.getElementById(`my_select_${inside_idx}`).value;
-    if (x == 1) {
+  function total_price_7(idx, item_price, all_items_price) {
+    var quantity = document.getElementById(`my_select_${idx}`).value;
+
+    if (quantity == 1) {
       price7 = 0;
     } else {
-      price7 = item_price * x - item_price;
+      price7 = item_price * quantity - item_price;
     }
     total_sums = all_items_price + price1 + price2 + price3 + price4 + price5 + price6 + price7 + price8 + price9 + price10;
-    document.getElementById("quantity_price").innerHTML = total_sums;
+    document.getElementById("quantity_price").innerHTML = "$" + total_sums.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
   }
 
-  function total_price_8(inside_idx, item_price, all_items_price) {
-    var x = document.getElementById(`my_select_${inside_idx}`).value;
-    if (x == 1) {
+  function total_price_8(idx, item_price, all_items_price) {
+    var quantity = document.getElementById(`my_select_${idx}`).value;
+
+    if (quantity == 1) {
       price8 = 0;
     } else {
-      price8 = item_price * x - item_price;
+      price8 = item_price * quantity - item_price;
     }
     total_sums = all_items_price + price1 + price2 + price3 + price4 + price5 + price6 + price7 + price8 + price9 + price10;
-    document.getElementById("quantity_price").innerHTML = total_sums;
+    document.getElementById("quantity_price").innerHTML = "$" + total_sums.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
   }
 
-  function total_price_9(inside_idx, inside_price, all_items_price) {
-    var x = document.getElementById(`my_select_${inside_idx}`).value;
-    if (x == 1) {
+  function total_price_9(idx, inside_price, all_items_price) {
+    var quantity = document.getElementById(`my_select_${idx}`).value;
+
+    if (quantity == 1) {
       price9 = 0;
     } else {
-      price9 = item_price * x - item_price;
+      price9 = item_price * quantity - item_price;
     }
     total_sums = all_items_price + price1 + price2 + price3 + price4 + price5 + price6 + price7 + price8 + price9 + price10;
-    document.getElementById("quantity_price").innerHTML = total_sums;
+    document.getElementById("quantity_price").innerHTML = "$" + total_sums.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
   }
 
-  function total_price_10(inside_idx, item_price, all_items_price) {
-    var x = document.getElementById(`my_select_${inside_idx}`).value;
-    if (x == 1) {
+  function total_price_10(idx, item_price, all_items_price) {
+    var quantity = document.getElementById(`my_select_${idx}`).value;
+
+    if (quantity == 1) {
       price10 = 0;
     } else {
-      price10 = item_price * x - item_price;
+      price10 = item_price * quantity - item_price;
     }
     total_sums = all_items_price + price1 + price2 + price3 + price4 + price5 + price6 + price7 + price8 + price9 + price10;
-    document.getElementById("quantity_price").innerHTML = total_sums;
+    document.getElementById("quantity_price").innerHTML = "$" + total_sums.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
   }
+
+
 
 
 
