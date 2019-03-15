14b51806766a43895dad843359c80e957fa17e95
Cheng0315 <csalvue@gmail.com>
2019-03-15 04:45:05 -0700

    add create action in review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,39 @@
 class ReviewsController < ApplicationController 
-  
+  before_action :set_item, only: [:new, :create]
+
   def new
-    @review = Review.new
+    if current_user
+      @review = Review.new
+    else
+      redirect_to root_path
+    end
+  end
+
+  def create 
+    if current_user
+      @review = Review.new(reviews_params)
+      @review.user_id = current_user.id
+      @review.item_id = params[:item_id]
+
+      if @review.save
+        redirect_to review_path(@review)
+      else
+        render :new
+      end
+    else
+      redirect_to root_path
+    end
+
   end
 
 
   private
 
-  def reviewss_params
-    params.require(:review).permit(:user_id, :item_id)
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def reviews_params
+    params.require(:review).permit(:user_id, :item_id, :rating, :comment)
   end
 end
