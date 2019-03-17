439dd7b84da1e032f751ff2dec8dce61e3143979
Cheng0315 <csalvue@gmail.com>
2019-03-17 10:53:48 -0700

    delete onClick on select quantity to use jquery instead

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,29 +18,60 @@
 
 
 
-  let price1 = 0;
-  let price2 = 0;
-  let price3 = 0;
-  let price4 = 0;
-  let price5 = 0;
-  let price6 = 0;
-  let price7 = 0;
-  let price8 = 0;
-  let price9 = 0;
-  let price10 = 0;
   
-  function total_price(idx, item_price, all_items_price) {
-    let quantity = document.getElementById(`my_select_${idx}`).value;
-    let quantity_price = item_price * quantity - item_price;
 
-    if (quantity == 1) {
-      eval('price' + (idx + 1) + '= 0');
-    } else {
-      eval('price' + (idx + 1) + `= ${quantity_price}`);
-    }
-    total_sums = all_items_price + price1 + price2 + price3 + price4 + price5 + price6 + price7 + price8 + price9 + price10;
-    document.getElementById("quantity_price").innerHTML = "$" + total_sums.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
-  }
+  $(document).on('turbolinks:load', function(){
+    let price1 = 0;
+    let price2 = 0;
+    let price3 = 0;
+    let price4 = 0;
+    let price5 = 0;
+    let price6 = 0;
+    let price7 = 0;
+    let price8 = 0;
+    let price9 = 0;
+    let price10 = 0;
+
+    let totalQuantity = $("#num-of-items").text();
+
+    $(".select-quantity").change(function() {
+      let idx = parseFloat($(this).attr('data-idx'));
+      let itemPrice = parseFloat($(this).attr('data-price'));
+      let totalPrice = parseFloat($(this).attr('data-total-price'));
+      let quantity = parseFloat($('#' + $(this).attr('id')).val());
+      let quantityPrice = itemPrice * quantity - itemPrice;
+      
+      if (quantity == 1) {
+        eval('price' + (idx + 1) + '= 0');
+      } else {
+        eval('price' + (idx + 1) + `= ${quantityPrice}`);
+      }
+
+      totalSums = totalPrice + price1 + price2 + price3 + price4 + price5 + price6 + price7 + price8 + price9 + price10;
+      $("#quantity_price").text("$" + totalSums.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,'))
+
+      
+    })
+
+    /*function total_price(idx, item_price, all_items_price) {
+      let quantity = document.getElementById(`my_select_${idx}`).value;
+      let quantity_price = item_price * quantity - item_price;
+  
+      if (quantity == 1) {
+        eval('price' + (idx + 1) + '= 0');
+      } else {
+        eval('price' + (idx + 1) + `= ${quantity_price}`);
+      }
+      total_sums = all_items_price + price1 + price2 + price3 + price4 + price5 + price6 + price7 + price8 + price9 + price10;
+      document.getElementById("quantity_price").innerHTML = "$" + total_sums.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+      
+      onchange: on_change_funct(idx, item['price']
+      
+      console.log(quantity)
+    }*/
+  })
+  
+  
 
 $(document).on('turbolinks:load', function(){
 
