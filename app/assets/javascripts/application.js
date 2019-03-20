9350c41f8f9b3a84fbb7263d9ec02068c863279c
Cheng0315 <csalvue@gmail.com>
2019-03-20 04:05:49 -0700

    add jquery to make navbar dropdown active when the user hover over the dropdown

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -98,6 +98,25 @@ function enableAndDisableBtn(review_id, total_reviews) {
     })
   })
 
+$(document).on('turbolinks:load', function(){
+
+  function toggleDropdown (e) {
+    const _d = $(e.target).closest('.dropdown'),
+      _m = $('.dropdown-menu', _d);
+    setTimeout(function(){
+      const shouldOpen = e.type !== 'click' && _d.is(':hover');
+      _m.toggleClass('show', shouldOpen);
+      _d.toggleClass('show', shouldOpen);
+      $('[data-toggle="dropdown"]', _d).attr('aria-expanded', shouldOpen);
+    }, e.type === 'mouseleave' ? 300 : 0);
+  }
+  
+  $('body')
+    .on('mouseenter mouseleave','.dropdown',toggleDropdown)
+    .on('click', '.dropdown-menu a', toggleDropdown);
+
+})
+
 
   $(document).on('turbolinks:load', function(){
     
