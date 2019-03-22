f0621bbc14cf5f993c15e7e91e5e4056c0a68eca
Cheng0315 <csalvue@gmail.com>
2019-03-22 04:39:01 -0700

    fix bug where shopping cart is not updating the total number of items when user click add to cart

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -231,7 +231,8 @@ $(document).on('turbolinks:load', function(){
       url: this.action,
       data: $(this).serialize()
     }).done(function(items_count) {
-      $('#num-of-items').text(items_count)
+      console.log(items_count)
+      $('.num-of-items').text(items_count)
     })
       
     $(`form.button_to button#${id}`).html("<i class='fas fa-spinner fa-fw fa-spin'></i>&nbsp Processing");
