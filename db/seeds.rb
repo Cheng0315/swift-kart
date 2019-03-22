5057a339c7ead13f05c27db344cf133ea5b6fd77
Cheng0315 <csalvue@gmail.com>
2019-03-22 15:28:01 -0700

    add css flex to shorter length add to cart btn, edit btn, and delete btn

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,7 +34,7 @@ joseph = User.create(first_name: 'Joseph', last_name: 'Peterson', seller: true, 
 michaels_items = [['Rare 1940 Antique Gramophone', 5299.99, 'Rare 1940 antique gramophone, great condition.', true, 1, 2, "app/assets/images/items_images/gramophone.png", "gramophone.png"],
   ['SIEMENS Washing Machine', 599.99, '2015 SIEMENS Washing machine, works great!', true, 2, 2, "app/assets/images/items_images/washer.png", "washer.png"],
   ['Multicolor Abstract Painting', 199.99, 'Painted by one of the most renowned artist in modern time.', false, 3, 1, "app/assets/images/items_images/painting_1.png", "painting_1.png"],
-  ['Assorted Color Plastic Toys', 14.99, 'Great for kids ages 3+', true, 4, 1, "app/assets/images/items_images/toys_1.png", "toys_1.png"],
+  ['Assorted Color Plastic Toys', 14.99, 'Great for kids age 3+', true, 4, 1, "app/assets/images/items_images/toys_1.png", "toys_1.png"],
   ['Books', 99.99, "Zero to One, Ego is the Enemy, The Obstacle is the Way, Exponential Organization, The Story of Innovation, Value Proposition Design, The Startup Owner's Manual, and The Corporate Startup" , false, 5, 2, "app/assets/images/items_images/books_1.png", "books_1.png"],
   ['Black Sony Alpha DSLR Camra', 1299.99, 'Rated as one of the best DSLR camra out there.', true, 6, 1, "app/assets/images/items_images/camra.png", "camra.png"],
   ['2016 Black and Blue Mustang', 25000, 'Great condition, low mileage.', true, 7, 2, "app/assets/images/items_images/mustang.png", "mustang.png"]]
