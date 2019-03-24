1394b1fb827e33e8ccfdeaf90bee8031de00fb41
Cheng0315 <csalvue@gmail.com>
2019-03-24 09:21:00 -0700

    move add to cart jquery to it own js file

---
+++ app/assets/javascripts/addToCart.js
@@ -0,0 +1,18 @@
+$(document).on('turbolinks:load', function(){
+  $(".add-to-cart-btn").parent().on('click', function(event) {
+    const id = $(this).children('button').attr('id');
+    
+    $.ajax({
+      type: "POST",
+      url: this.action,
+      data: $(this).serialize()
+    }).done(function(items_count) {
+      $('.num-of-items').text(items_count)
+    })
+      
+    $(`form.button_to button#${id}`).html("<i class='fas fa-spinner fa-fw fa-spin'></i>&nbsp Processing");
+    setTimeout( function() {$(`form.button_to button#${id}`).html("<i class='fas fa-check'>&nbsp</i> Add to cart"); },450);
+    
+    event.preventDefault();
+  });
+});
