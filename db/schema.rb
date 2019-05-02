cf34e536588273af1062f3ea55056e7bcc5e1c8c
Cheng0315 <csalvue@gmail.com>
2019-05-02 13:25:07 -0700

    add delete sqlite, add pg as database

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2019_03_15_101227) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -34,8 +37,8 @@ ActiveRecord::Schema.define(version: 2019_03_15_101227) do
   end
 
   create_table "cart_items", force: :cascade do |t|
-    t.integer "cart_id"
-    t.integer "item_id"
+    t.bigint "cart_id"
+    t.bigint "item_id"
     t.integer "quantity"
     t.boolean "shipped", default: false
     t.datetime "created_at", null: false
@@ -45,7 +48,7 @@ ActiveRecord::Schema.define(version: 2019_03_15_101227) do
   end
 
   create_table "carts", force: :cascade do |t|
-    t.integer "user_id"
+    t.bigint "user_id"
     t.decimal "total_price"
     t.boolean "checkout", default: false
     t.datetime "created_at", null: false
@@ -66,8 +69,8 @@ ActiveRecord::Schema.define(version: 2019_03_15_101227) do
     t.integer "condition"
     t.boolean "in_stock"
     t.boolean "shipped"
-    t.integer "user_id"
-    t.integer "category_id"
+    t.bigint "user_id"
+    t.bigint "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_items_on_category_id"
@@ -77,8 +80,8 @@ ActiveRecord::Schema.define(version: 2019_03_15_101227) do
   create_table "reviews", force: :cascade do |t|
     t.integer "rating"
     t.text "comment"
-    t.integer "user_id"
-    t.integer "item_id"
+    t.bigint "user_id"
+    t.bigint "item_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["item_id"], name: "index_reviews_on_item_id"
@@ -96,4 +99,5 @@ ActiveRecord::Schema.define(version: 2019_03_15_101227) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end
